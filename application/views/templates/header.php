606f213457926bcf735f64eaa4950c4a2b51965b
Soni Julianto <sonijulianto@gmail.com>
2021-03-21 23:18:51 +0700

    add template

---
+++ application/views/templates/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Fundraiser &mdash; Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Mansalva|Roboto&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/animate.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/style.css">
+
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+
+    <div class="site-wrap" id="home-section">
+
+        <div class="site-mobile-menu site-navbar-target">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+
+
+
+
+
+
+        <header class="site-navbar site-navbar-target bg-secondary shadow" role="banner">
+
+            <div class="container">
+                <div class="row align-items-center position-relative">
+
+
+                    <div class="site-logo">
+                        <a href="index.html" class="text-white">Fundraiser</a>
+                    </div>
+
+
+                    <nav class="site-navigation text-left ml-auto " role="navigation">
+                        <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                            <li class="active"><a href="index.html" class="nav-link">Home</a></li>
+                            <li><a href="about.html" class="nav-link">About Us</a></li>
+                            <li><a href="causes.html" class="nav-link">Our Causes</a></li>
+                            <li><a href="blog.html" class="nav-link">Blog</a></li>
+                            <li><a href="contact.html" class="nav-link">Contact</a></li>
+                        </ul>
+                    </nav>
+
+
+                    <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
+
+
+
+                </div>
+            </div>
+
+        </header>
